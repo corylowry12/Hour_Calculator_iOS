8a62dabf103e221077ab1ac5e65ac03c60833e1a
Cory Lowry <corylowry12@gmail.com>
2021-06-22 13:21:54 -0500

    Initial Commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HourCalculator
+//
+//  Created by Cory Lowry on 6/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
