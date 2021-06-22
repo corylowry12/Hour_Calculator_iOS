4e59c9d8d6944284cd298d196d5d87796e115c09
Cory Lowry <corylowry12@gmail.com>
2021-06-22 14:46:19 -0500

    Home Screen Changes

--- HourCalculator/HourCalculator/ViewController.swift
+++ HourCalculator/HourCalculator/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func datePickerChanged(_ sender: Any) {
+        
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        
+        let strDate = dateFormatter.string(from: datePicker.date)
+        dateLabel.text = strDate
+    }
+    
 }
 
