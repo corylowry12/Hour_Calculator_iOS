b8fc930ae0eadeb9794a23c3c627bba195dc52bd
Cory Lowry <corylowry12@gmail.com>
2021-07-03 20:21:08 -0500

    Removed dependencies

--- HourCalculator/HourCalculator/AppDelegate.swift
+++ HourCalculator/HourCalculator/AppDelegate.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 import GoogleMobileAds
-import Firebase
 import CoreData
 import Siren
 
@@ -18,8 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        FirebaseApp.configure()
         
         GADMobileAds.sharedInstance().start(completionHandler: nil)
         
