4198f23eba39092f4e7f0d6e5b978db5e8b9f14a
Cory Lowry <corylowry12@gmail.com>
2021-08-20 01:27:31 -0500

    Added image view to select image for time card item

--- HourCalculator/HourCalculator/View Controllers/TimeCardInfoTableViewController.swift
+++ HourCalculator/HourCalculator/View Controllers/TimeCardInfoTableViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 import CoreData
 import GoogleMobileAds
 
-class TimeCardInfoTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+class TimeCardInfoTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
     lazy var bannerView: GADBannerView! = GADBannerView(adSize: kGADAdSizeBanner)
     
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var totalHoursLabel: UILabel!
     @IBOutlet weak var weekOfLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     let userDefaults = UserDefaults.standard
+    
+    var imagePosition : Int!
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
@@ -135,6 +138,127 @@ class TimeCardInfoTableViewController: UIViewController, UITableViewDataSource, 
         }
         textField.tintColor = UIColor.systemGray2
         view.backgroundColor = tableView.backgroundColor
+        
+        imageView.isUserInteractionEnabled = true
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
+        imageView.addGestureRecognizer(tapRecognizer)
+        
+        /*for i in 0...timeCard.count - 1 {
+           let image = timeCard[i].image
+            if image == nil {
+                imageView.backgroundColor = UIColor.red
+            }
+            else {
+                imageView.image = UIImage(data: timeCard[i].image!)
+            }
+        }*/
+        
+        if timeCard[timeCard.count - 1].image != nil {
+            imageView.image = UIImage(data: timeCard[timeCard.count - 1].image!)
+            //imagePosition = userDefaults.integer(forKey: "id")
+            imageView.backgroundColor = UIColor.clear
+        }
+        else {
+            setImageView()
+        }
+    }
+    
+    func setImageView() {
+        if userDefaults.integer(forKey: "accent") == 0 {
+            imageView.image = UIImage(named: "teal_logo")
+        }
+        else if userDefaults.integer(forKey: "accent") == 1 {
+            textField.backgroundColor = UIColor(rgb: 0x7841c4)
+            imageView.image = UIImage(named: "purple_logo")
+        }
+        else if userDefaults.integer(forKey: "accent") == 2 {
+            imageView.image = UIImage(named: "blue_logo")
+            
+        }
+        else if userDefaults.integer(forKey: "accent") == 3 {
+            imageView.image = UIImage(named: "orange_logo")
+        }
+        else if userDefaults.integer(forKey: "accent") == 4 {
+            imageView.image = UIImage(named: "red_logo")
+        }
+    }
+    
+    @objc func imageTapped() {
+        print("image Tapped")
+        if timeCard[timeCard.count - 1].image != nil {
+            print("hello world")
+            let alert = UIAlertController(title: "Warning", message: "There is already an image stored. What would you like to do?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "View Image", style: .default, handler: { [self] _ in
+               
+                let image = UIImage(data: timeCard[timeCard.count - 1].image!)
+                let showAlert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+                let imageView = UIImageView(frame: CGRect(x: 10, y: 50, width: 250, height: 230))
+                imageView.contentMode = .scaleAspectFit
+                imageView.image = image // Your image here...
+                showAlert.view.addSubview(imageView)
+                let height = NSLayoutConstraint(item: showAlert.view!, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 320)
+                let width = NSLayoutConstraint(item: showAlert.view!, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 250)
+                showAlert.view.addConstraint(height)
+                showAlert.view.addConstraint(width)
+                showAlert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+                self.present(showAlert, animated: true, completion: nil)
+            }))
+            alert.addAction(UIAlertAction(title: "Remove It", style: .default, handler: { [self] _ in
+                setImageView()
+                timeCard[timeCard.count - 1].image = nil
+                (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            }))
+            alert.addAction(UIAlertAction(title: "Choose a new image", style: .default, handler: { _ in
+                let alert = UIAlertController(title: "Choose a new image", message: "What would you like to do?", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Take a photo", style: .default, handler: { _ in
+                    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                                let imagePickerController = UIImagePickerController()
+                                imagePickerController.delegate = self;
+                                imagePickerController.sourceType = .camera
+                                self.present(imagePickerController, animated: true, completion: nil)
+                            }
+                }))
+                alert.addAction(UIAlertAction(title: "Choose a photo", style: .default, handler: { _ in
+                    if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+                        let imagePickerController = UIImagePickerController()
+                                    imagePickerController.delegate = self;
+                                    imagePickerController.sourceType = .photoLibrary
+                                    self.present(imagePickerController, animated: true, completion: nil)
+                    }
+                }))
+                self.present(alert, animated: true, completion: nil)
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    let imagePickerController = UIImagePickerController()
+                    imagePickerController.delegate = self;
+                    imagePickerController.sourceType = .camera
+                    self.present(imagePickerController, animated: true, completion: nil)
+                }
+        else {
+            let imagePickerController = UIImagePickerController()
+                        imagePickerController.delegate = self;
+                        imagePickerController.sourceType = .photoLibrary
+                        self.present(imagePickerController, animated: true, completion: nil)
+        }
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        imageView.image = image
+        imageView.backgroundColor = UIColor.clear
+        self.dismiss(animated: true, completion: nil)
+        
+        let jpegData = image?.jpegData(compressionQuality: 1.0)
+        timeCard[timeCard.count - 1].image = jpegData
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -190,18 +314,17 @@ class TimeCardInfoTableViewController: UIViewController, UITableViewDataSource, 
         let hourItems = timeCard[indexPath.row]
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "timeCardCell", for: indexPath) as! TimeCardInfoTableViewCell
-        //DispatchQueue.main.async {
+       
         let inTime = hourItems.intime
         let outTime = hourItems.outtime
         let totalHours = hourItems.total_hours
         let date = hourItems.date!
         
-        
         cell.inTimeLabel.text = "In Time: \(inTime ?? "Unknown")"
         cell.outTimeLabel.text = "Out Time: \(outTime ?? "Unknown")"
         cell.totalHoursLabel.text = "Total Hours: \(totalHours ?? "Unknown")"
         cell.dateLabel.text = "Date: \(date)"
-        //}
+        
         return cell
     }
     @IBAction func textFieldTextChanged(_ sender: UITextField) {
